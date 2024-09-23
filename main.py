413162c3f04885dc85876977215118cf6a162426
pedro092692 <pedro_092692@hotmail.com>
2024-09-22 22:37:30 -0400

    add demo pdf for test

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from txt_to_audio import TextToAudio
 from pdf_reader import PdfToText
 
+"""
+Your pdf file must be named 'pdf.pdf'
+"""
+
 text_to_audio = TextToAudio()
 pdf_file = PdfToText()
 text = pdf_file.get_text()
--- pdf_reader.py
+++ pdf_reader.py
@@ -4,7 +4,11 @@ from PyPDF2 import PdfReader
 class PdfToText:
 
     def __init__(self):
-        self.reader = PdfReader('example.pdf')
+        try:
+            self.reader = PdfReader('pdf.pdf')
+        except FileNotFoundError:
+            print('Your pdf file must be named "pdf.pdf"')
+            exit(0)
 
     def get_text(self):
         text = ''
